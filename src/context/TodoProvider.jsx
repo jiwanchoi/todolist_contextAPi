fc2453623ba9df0756e7a46d3218bcbb450a5ecb
jiwanchoi <wldhks303@naver.com>
2023-10-18 10:33:15 +0900

    :zap: Login button
    login button add

--- src/context/TodoProvider.jsx
+++ src/context/TodoProvider.jsx
@@ -1,29 +1,28 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState } from "react";
 
 const TodoContext = createContext();
 
-const initialTodoListState = [""];
+const initialTodoListState = ["바주카포"];
 
-export default function TodoProvider({children}) {
-    const [todoList, setTodoList] = useState(initialTodoListState);
+export default function TodoProvider({ children }) {
+  const [todoList, setTodoList] = useState(initialTodoListState);
 
+  const addTodo = (newTodoItem) => {
+    setTodoList([...todoList, newTodoItem]);
+  };
 
-    const addTodo=(newTodoItem)=>{
-      setTodoList([...todoList, newTodoItem]);
-    }
+  const removeTodo = (todoIndex) => {
+    const newList = todoList.filter((_, index) => index !== todoIndex);
+    setTodoList(newList);
+  };
 
-    const removeTodo=(todoIndex)=>{
-      const newList = todoList.filter((_,index)=>index !==todoIndex);
-      setTodoList(newList);
-    }
-
-    const contextValue = {
-        todoList,
-        addTodo,
-        removeTodo,
-    };
+  const contextValue = {
+    todoList,
+    addTodo,
+    removeTodo,
+  };
   return (
-    <TodoContext.Provider value= {contextValue}>{children}</TodoContext.Provider>
-  )
+    <TodoContext.Provider value={contextValue}>{children}</TodoContext.Provider>
+  );
 }
 export const useTodoContext = () => useContext(TodoContext);
